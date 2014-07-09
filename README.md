323d976c40189082e5409f360f2db576d9fa281a
allanino <allaninocencio@yahoo.com.br>
2014-07-09 15:42:09 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+Sci-Hub-Fy
+==========
+
+Chrome extension that appends ".sci-hub.org" to active tab domain, allowing free access to scientific articles.
+
+The hard work is done by [Sci-Hub].
+
+[Sci-Hub]:http://sci-hub.org
---
+++ background.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2014 Allan Costa.
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var arr = tab.url.split('/');
+  arr[2] = arr[2] + ".sci-hub.org";
+  var new_url = arr.join('/');
+  
+  console.log('Redirecting ' + tab.url + ' to ' + new_url);
+
+  chrome.tabs.update(tab.id, {url: new_url});
+});

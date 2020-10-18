91629ba4062e190f70aba950f273be43a0740911
Marcos Bressan <bressanmarcos@alu.ufc.br>
2020-10-18 10:32:25 -0300

    Fix: Change references to most recent domain .st

--- options.js
+++ options.js
@@ -7,9 +7,9 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value domain = 'sci-hub.tw'
+  // Use default value domain = 'sci-hub.st'
   chrome.storage.sync.get({
-    domain: 'sci-hub.tw'
+    domain: 'sci-hub.st'
   }, function(items) {
     document.getElementById('domain').value = items.domain;
   });
